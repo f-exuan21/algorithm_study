3a5af6bfd98bdfeed70bfb7f65092b506fbe1558
luchia <88169983+happyfranc@users.noreply.github.com>
2021-10-17 15:03:05 +0900

    Add files via upload

---
+++ 08_다이나믹프로그래밍/8_다이나믹프로그래밍_박이정.py
@@ -0,0 +1,107 @@
+# 파보나치 함수(fibonacci Function)을 재귀 함수로 구현
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    return fibo(x - 1) + fibo(x - 2)
+print(fibo(4))
+
+d = [0] * 100
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+print(fibo(99))
+
+#호출되는 함수 확인
+d = [0] * 100
+
+def pibo(x):
+    print('f('+ str(x) + ')', end = ' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = pibo(x - 1) + pibo(x - 2)
+
+    return d[x]
+
+pibo(6)
+
+## 피보나치 수열 소스코드(반복적)
+d = [0] * 100
+
+d[1] = 1
+d[2] = 1
+n = 99
+
+for i in range(3, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+
+print(d[n])
+
+#1로 만들기
+x = int(input())
+
+d = [0] * 30001
+
+for i in range(2, x + 1):
+    d[i] = d[i - 1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+    if i  % 5 == 0:
+        d[i] = min(d[i], d[i // 5] + 1)
+
+print(d[x])
+
+#개미 전사
+n = int(input())
+array = list(map(int, input().split()))
+
+d = [0] * 100
+
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + array[i])
+
+print(d[n - 1])
+
+#바닥 공사
+n = int(input())
+
+d = [0] * 1001
+
+d[1] = 1
+d[2] = 3
+for i in range(3, n + 1):
+    d[i] = (d[i - 1] + 2 * d[i - 2]) % 796796
+
+print(d[n])
+
+# 효율적인 화폐 구성
+n, m = map(int, input().split())
+
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m + 1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
