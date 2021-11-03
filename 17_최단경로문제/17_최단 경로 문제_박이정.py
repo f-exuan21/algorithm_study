8a410e96c7790f481b58af8f4b94f359fa5b9d77
luchia <88169983+happyfranc@users.noreply.github.com>
2021-11-03 18:09:44 +0900

    Add files via upload

---
+++ 17_최단경로문제/17_최단 경로 문제_박이정.py
@@ -0,0 +1,151 @@
+# 37번
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if c < graph[a][b]:
+        graph[a][b] = c
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
+
+#38번 정확한 순위
+#A에서 B로 도달이 가능하거나, B에서 A로 도달이 가능하면 '성적 비교'가 가능한 것
+
+INF = int(1e9)
+
+n,m = map(int, input().split())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+result = 0
+
+for i in range(1, n+1):
+    count = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    if count == n:
+        result += 1
+print(result)
+
+#39번 화성 탐사
+#(0,0)에서 (N - 1, N - 1)의 위치로 이동하는 최단 거리를 계산하는 문제
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+for tc in range(int(input())):
+    n = int(input())
+
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int, input().split())))
+    distance = [[INF] * n for _ in range(n)]
+
+    q = [(graph[x][y], x, y)]
+    distance[x][y] = graph[x][y]
+
+    while q:
+        dist, x, y = heapq.heappop(q)
+        if distance[x][y] < dist:
+            continue
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            cost = dist + graph[nx][ny]
+
+            if cost < distance[nx][ny]:
+                distance[nx][ny] = cost
+                heapq.heappush(q,(cost, nx, ny))
+
+    print(distance[n - 1][n - 1])
+
+#40번 숨바꼭질
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = 1
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+    graph[b].append((a, 1))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+max_node = 0
+
+max_distance = 0
+
+result = []
+
+for i in range(1, n+1):
+    if max_distance < distance[i]:
+        max_node = i
+        max_distance = distance[i]
+        result = [max_node]
+    elif max_distance == distance[i]:
+        result.append(i)
+print(max_node, max_distance, len(result))
